dd96ddcd99b5bdd28a16983841258ba2251d34bf
Pavinthan <pavinthan@outlook.com>
2019-09-29 23:22:09 +0530

    merge with master

--- src/components/LayoutHeader/Header.js
+++ src/components/LayoutHeader/Header.js
@@ -123,28 +123,6 @@ const Header = ({location}: {location: Location}) => (
                 'linear-gradient(to right, transparent, black 20px, black 90%, transparent)',
             },
           }}>
-<<<<<<< HEAD
-          <HeaderLink
-            isActive={location.pathname.includes('/docs/')}
-            title="ஆவணங்கள்"
-            to="/docs/getting-started.html"
-          />
-          <HeaderLink
-            isActive={location.pathname.includes('/tutorial/')}
-            title="பயிற்சிகள்"
-            to="/tutorial/tutorial.html"
-          />
-          <HeaderLink
-            isActive={location.pathname.includes('/blog')}
-            title="வலைப்பதிவு"
-            to="/blog/"
-          />
-          <HeaderLink
-            isActive={location.pathname.includes('/community/')}
-            title="சமூகம்"
-            to="/community/support.html"
-          />
-=======
           {navHeader.items.map(link => (
             <HeaderLink
               key={link.title}
@@ -153,7 +131,6 @@ const Header = ({location}: {location: Location}) => (
               to={link.to}
             />
           ))}
->>>>>>> 8b30ae43439818009213da86eca872a3d869c5eb
         </nav>
 
         <DocSearch />
